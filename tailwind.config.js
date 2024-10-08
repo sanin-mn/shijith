6701619bbe422df625d7b2f0dbad27990eec18ed
sanin-mn <msanin7003@gmail.com>
2024-10-08 13:09:13 +0530

    inintial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,jsx,ts,tsx}", // Ensure this matches your file structure
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
